6c9b3e42e255a602df6a455330d66eab61254364
Jhanavi-220 <83897993+Jhanavi-220@users.noreply.github.com>
2021-06-14 11:37:10 +0530

    Add files via upload

---
+++ LAB-5/Vowel or consonant using switch case.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+ int main()
+ {
+ char ch;
+ printf("Enter any alphabet: ");
+ scanf("%c", &ch);
+ switch(ch)
+ {
+ case 'a':
+
+ case 'e':
+
+ case 'i':
+
+ case 'o':
+
+ case 'u':
+ {
+  printf("Vowel");
+ }
+  break;
+ case 'A':
+
+ case 'E':
+
+ case 'I':
+
+ case 'O':
+
+ case 'U':
+ {
+  printf("Vowel");
+ }
+  break;
+ default:
+ {
+  printf("Consonant");
+ }
+}
+return 0;
+}
